92b8df0965a4dfcb04f744fcfe858f1e2fae233f
kocyusuf <koccyusuff@gmail.com>
2019-11-21 13:22:47 +0300

    websitesi

---
+++ js/dummydata.js
@@ -0,0 +1,7 @@
+// grid dummy data
+var dummyData = {
+	"Bitirme Tezi":"<li class=\"product\"><div class=\"foodicon foodicon--broccoli\"></div></li>"
+	"Mühendislik Projesi":"<li class=\"product\"><div class=\"foodicon foodicon--grain\"></div></li>"
+	"Karayolu Projesi":"<li class=\"product\"><div class=\"foodicon foodicon--lettuce\"></div></li>"
+	
+}
